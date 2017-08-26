7657d77fe2af5759c3c8aad78c383d1a0cb62bcb
Harri Feng <harrifeng@gmail.com>
2017-08-26 21:35:23 +0800

    try again

--- 01-utils/prepare.sh
+++ 01-utils/prepare.sh
@@ -1,4 +1,5 @@
 chmod 600 /home/vagrant/.ssh/id_rsa
 sudo yum install -y epel-release
-sudo yum install -y python-devel python-pip gcc
+sudo yum install -y python-devel python-pip gcc yum-utils
 sudo pip install ansible -i http://mirrors.aliyun.com/pypi/simple --trusted-host=mirrors.aliyun.com
+yumdownloader --resolve --destdir=/vagrant/yml/httpd python-pip libffi gcc openssl libselinux-python ca-certificates ntp python-devel
