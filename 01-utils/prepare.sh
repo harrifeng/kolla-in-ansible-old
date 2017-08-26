1840cd9d465d0a38809f709b5790ebfbfe1c4b1c
Harri Feng <harrifeng@gmail.com>
2017-08-26 22:18:47 +0800

    better name

--- 01-utils/prepare.sh
+++ 01-utils/prepare.sh
@@ -2,5 +2,5 @@ chmod 600 /home/vagrant/.ssh/id_rsa
 sudo yum install -y epel-release
 sudo yum install -y python-devel python-pip gcc yum-utils createrepo
 sudo pip install ansible -i http://mirrors.aliyun.com/pypi/simple --trusted-host=mirrors.aliyun.com
-yumdownloader --resolve --destdir=/vagrant/yml/httpd/yum python-pip libffi gcc openssl libselinux-python ca-certificates ntp python-devel
-createrepo /vagrant/yml/httpd/yum
+yumdownloader --resolve --destdir=/vagrant/yml/httpd/localrpm python-pip libffi gcc openssl libselinux-python ca-certificates ntp python-devel
+createrepo /vagrant/yml/httpd/localrpm
