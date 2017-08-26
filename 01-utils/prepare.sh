8253a0759caab79cc59cb964c4c305e10b18fdc8
Harri Feng <harrifeng@gmail.com>
2017-08-26 20:15:01 +0800

    new mode for id file

--- 01-utils/prepare.sh
+++ 01-utils/prepare.sh
@@ -1,3 +1,4 @@
+chmod 600 /home/vagrant/.ssh/id_rsa
 sudo yum install -y epel-release
 sudo yum install -y python-devel python-pip gcc
 sudo pip install ansible -i http://mirrors.aliyun.com/pypi/simple --trusted-host=mirrors.aliyun.com
