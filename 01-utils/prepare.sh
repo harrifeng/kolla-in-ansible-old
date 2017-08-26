e331729a91266108751234f23f38333d69a84aa8
Harri Feng <harrifeng@gmail.com>
2017-08-26 19:39:46 +0800

    ok need config file

--- 01-utils/prepare.sh
+++ 01-utils/prepare.sh
@@ -1,1 +1,3 @@
-sudo mkdir -p /root/.ssh
+sudo yum install -y epel-release
+sudo yum install -y python-devel python-pip gcc
+sudo pip install ansible -i http://mirrors.aliyun.com/pypi/simple --trusted-host=mirrors.aliyun.com
