a9b2aa94557aa406a6ba43058d04a54d3462da0d
Harri Feng <harrifeng@gmail.com>
2017-08-27 19:14:45 +0800

    new repo ready

--- 01-utils/yml/httpd/download_rpm.sh
+++ 01-utils/yml/httpd/download_rpm.sh
@@ -1,1 +1,1 @@
-yumdownloader --resolve --destdir=/vagrant/yml/httpd/localrpm ca-certificates docker-ce gcc libffi-devel openssl python-devel python-pip libselinux-python ntp
+yumdownloader --resolve --destdir=/vagrant/yml/httpd/localrpm ca-certificates docker-ce gcc libffi-devel openssl python-backports python-backports-ssl_match_hostname python-devel python-pip  python-setuptools libselinux-python ntp
