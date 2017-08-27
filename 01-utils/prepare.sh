6c66a702ec552b4f936635687dc31d3b096029e2
Harri Feng <harrifeng@gmail.com>
2017-08-27 15:54:55 +0800

    generate for docker

--- 01-utils/prepare.sh
+++ 01-utils/prepare.sh
@@ -1,6 +1,7 @@
 chmod 600 /home/vagrant/.ssh/id_rsa
 sudo yum install -y epel-release yum-utils createrepo
-yumdownloader --resolve --destdir=/vagrant/yml/httpd/localrpm python-pip libffi gcc openssl libselinux-python ca-certificates ntp python-devel container-selinux libseccomp libtool-ltdl
+sudo yum install -y /vagrant/yml/httpd/localrpm/docker-ce-17.06.0.ce-1.el7.centos.x86_64.rpm
+yumdownloader --resolve --destdir=/vagrant/yml/httpd/localrpm docker python-pip libffi-devel gcc openssl libselinux-python ca-certificates ntp python-devel container-selinux libseccomp libtool-ltdl
 createrepo /vagrant/yml/httpd/localrpm
 sudo yum install -y python-devel python-pip gcc
 sudo pip install ansible -i http://mirrors.aliyun.com/pypi/simple --trusted-host=mirrors.aliyun.com
