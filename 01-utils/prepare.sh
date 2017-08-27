1664173b7fba71de0493c48f76474071fd5ee74b
Harri Feng <harrifeng@gmail.com>
2017-08-27 14:52:00 +0800

    ok

--- 01-utils/prepare.sh
+++ 01-utils/prepare.sh
@@ -1,6 +1,17 @@
 chmod 600 /home/vagrant/.ssh/id_rsa
 sudo yum install -y yum-utils
-yumdownloader --resolve --destdir=/vagrant/yml/httpd/localrpm python-pip libffi gcc openssl libselinux-python ca-certificates ntp python-devel
+yumdownloader --resolve --destdir=/vagrant/yml/httpd/localrpm
+python-pip \
+    libffi \
+    gcc \
+    openssl \
+    libselinux-python \
+    ca-certificates \
+    ntp \
+    python-devel \
+    container-selinux \
+    libseccomp \
+    libtool-ltdl
 createrepo /vagrant/yml/httpd/localrpm
 sudo yum install -y epel-release
 sudo yum install -y python-devel python-pip gcc createrepo
