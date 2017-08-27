7cd07dbdac03d7a77c052e78650b841a06d58586
Harri Feng <harrifeng@gmail.com>
2017-08-27 18:09:15 +0800

    to gen it

---
+++ 01-utils/yml/httpd/download_rpm.sh
@@ -0,0 +1,9 @@
+yumdownloader --resolve --destdir=/vagrant/yml/httpd/localrpm ca-certificates \
+docker-ce \
+gcc \
+libffi-devel \
+openssl \
+python-devel \
+python-pip \
+libselinux-python \
+ntp
