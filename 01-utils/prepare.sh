537bace10a9e6b411ca00bab66689fc4eaf99a68
Harri Feng <harrifeng@gmail.com>
2017-08-27 15:24:37 +0800

    ok to gen

--- 01-utils/prepare.sh
+++ 01-utils/prepare.sh
@@ -1,18 +1,6 @@
 chmod 600 /home/vagrant/.ssh/id_rsa
-sudo yum install -y yum-utils
-yumdownloader --resolve --destdir=/vagrant/yml/httpd/localrpm
-python-pip \
-    libffi \
-    gcc \
-    openssl \
-    libselinux-python \
-    ca-certificates \
-    ntp \
-    python-devel \
-    container-selinux \
-    libseccomp \
-    libtool-ltdl
+sudo yum install -y epel-release yum-utils createrepo
+yumdownloader --resolve --destdir=/vagrant/yml/httpd/localrpm python-pip libffi gcc openssl libselinux-python ca-certificates ntp python-devel container-selinux libseccomp libtool-ltdl
 createrepo /vagrant/yml/httpd/localrpm
-sudo yum install -y epel-release
-sudo yum install -y python-devel python-pip gcc createrepo
+sudo yum install -y python-devel python-pip gcc
 sudo pip install ansible -i http://mirrors.aliyun.com/pypi/simple --trusted-host=mirrors.aliyun.com
