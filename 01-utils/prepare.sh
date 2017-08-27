84089e523ffb666769baf202c4caa4ec2e0659a0
Harri Feng <harrifeng@gmail.com>
2017-08-27 16:22:07 +0800

    prepare not in provision

--- 01-utils/prepare.sh
+++ 01-utils/prepare.sh
@@ -1,7 +1,12 @@
 chmod 600 /home/vagrant/.ssh/id_rsa
 sudo yum install -y epel-release yum-utils createrepo
-sudo yum install -y /vagrant/yml/httpd/localrpm/docker-ce-17.06.0.ce-1.el7.centos.x86_64.rpm
-yumdownloader --resolve --destdir=/vagrant/yml/httpd/localrpm docker python-pip libffi-devel gcc openssl libselinux-python ca-certificates ntp python-devel container-selinux libseccomp libtool-ltdl
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+echo "get the local repo"
+yumdownloader --resolve --destdir=/vagrant/yml/httpd/localrpm docker-ce \
+              python-pip libffi-devel gcc openssl libselinux-python ca-certificates \
+              ntp python-devel container-selinux libseccomp libtool-ltdl device-mapper-persistent-data lvm2
 createrepo /vagrant/yml/httpd/localrpm
 sudo yum install -y python-devel python-pip gcc
 sudo pip install ansible -i http://mirrors.aliyun.com/pypi/simple --trusted-host=mirrors.aliyun.com
