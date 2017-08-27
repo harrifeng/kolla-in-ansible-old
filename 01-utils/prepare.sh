d6b5a918ebcf521d01606d74c9a057cb4e343d8f
Harri Feng <harrifeng@gmail.com>
2017-08-27 14:33:08 +0800

    need to yumdownloader at very first

--- 01-utils/prepare.sh
+++ 01-utils/prepare.sh
@@ -1,6 +1,7 @@
 chmod 600 /home/vagrant/.ssh/id_rsa
-sudo yum install -y epel-release
-sudo yum install -y python-devel python-pip gcc yum-utils createrepo
-sudo pip install ansible -i http://mirrors.aliyun.com/pypi/simple --trusted-host=mirrors.aliyun.com
+sudo yum install -y yum-utils
 yumdownloader --resolve --destdir=/vagrant/yml/httpd/localrpm python-pip libffi gcc openssl libselinux-python ca-certificates ntp python-devel
 createrepo /vagrant/yml/httpd/localrpm
+sudo yum install -y epel-release
+sudo yum install -y python-devel python-pip gcc createrepo
+sudo pip install ansible -i http://mirrors.aliyun.com/pypi/simple --trusted-host=mirrors.aliyun.com
