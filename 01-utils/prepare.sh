b2048c2da0a6821ff99c029266cdd809f04e9108
Haoran Feng <haoran.feng@alo7.com>
2017-08-28 09:17:11 +0800

    a workable edition

--- 01-utils/prepare.sh
+++ 01-utils/prepare.sh
@@ -1,4 +1,7 @@
 chmod 600 /home/vagrant/.ssh/id_rsa
 sudo yum install -y epel-release
+sudo yum install -y yum-utils createrepo
+createrepo /vagrant/yml/httpd/localrpm
 sudo yum install -y python-devel python-pip gcc
+echo "pip----------->"
 sudo pip install ansible -i http://mirrors.aliyun.com/pypi/simple --trusted-host=mirrors.aliyun.com
