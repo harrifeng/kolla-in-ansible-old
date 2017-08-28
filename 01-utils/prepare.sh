80e7a158753b2667cdfd892c33b657ca2c2272af
Haoran Feng <haoran.feng@alo7.com>
2017-08-28 09:24:28 +0800

    ok now

--- 01-utils/prepare.sh
+++ 01-utils/prepare.sh
@@ -1,7 +1,8 @@
 chmod 600 /home/vagrant/.ssh/id_rsa
 sudo yum install -y epel-release
 sudo yum install -y yum-utils createrepo
-createrepo /vagrant/yml/httpd/localrpm
 sudo yum install -y python-devel python-pip gcc
 echo "pip----------->"
+sudo pip install -U pip -i http://mirrors.aliyun.com/pypi/simple --trusted-host=mirrors.aliyun.com
 sudo pip install ansible -i http://mirrors.aliyun.com/pypi/simple --trusted-host=mirrors.aliyun.com
+echo "finishing provision--------------->"
--- 01-utils/yml/httpd/download_rpm.sh
+++ 01-utils/yml/httpd/download_rpm.sh
@@ -1,1 +1,3 @@
+# this file should be run only once if the rpm files in version control system are outdated
 yumdownloader --resolve --destdir=/vagrant/yml/httpd/localrpm ca-certificates docker-ce gcc libffi-devel openssl python-backports python-backports-ssl_match_hostname python-devel python-pip  python-setuptools libselinux-python ntp
+createrepo /vagrant/yml/httpd/localrpm
